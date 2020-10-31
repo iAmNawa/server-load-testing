e9f7226f609728874b28e4ff2945c7d67507d540
Paul Borawski <paulborawski14@gmail.com>
2020-10-31 15:21:37 -0400

    load tester writing to file

--- front-end/loadTest.js
+++ front-end/loadTest.js
@@ -1,19 +1,22 @@
 const axios = require('axios')
 const fs = require('fs')
 
-let success = 0
-let errors = 0
-let count = 0
+let final = {
+  success: 0,
+  errors: 0,
+  count: 0,
+  totalMilliseconds: 0
+}
 
 function oneTest() {
   axios.get('http://localhost:5000/why-am-i-so-slow')
     .then(res => {
-      success++
-      count++
+      final.success++
+      final.count++
     })
     .catch(err => {
-      errors++
-      count++
+      final.errors++
+      final.count++
     })
 }
 
@@ -29,10 +32,22 @@ function oneTestLoop() {
 oneTestLoop()
 
 let loadTestIsDone = setInterval(() => {
-  if (count === 1000) {
+  if (final.count === 1000) {
+    final.totalMilliseconds = Date.now() -  startTime
     clearInterval(loadTestIsDone)
-    console.log('success: ', success)
-    console.log('errors: ', errors)
-    console.log('how long: ', Date.now() -  startTime)
+    fs.appendFile(
+      './expressResults.txt',
+      JSON.stringify(final) + '\n' + '-----------------------' + '\n',
+      function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('the pizza is out of the oven')
+        }
+      }
+    )
+    console.log('success: ', final.success)
+    console.log('errors: ', final.errors)
+    console.log('how long: ', final.totalMilliseconds)
   }
 }, 10)
--- readme.md
+++ readme.md
@@ -5,4 +5,5 @@ testing speed of back-end servers and languages that are compatible with REST AP
 Install the packages from the package.json    
 You can only start one server at a time   
 You will probably need to install Flask with pip install Flask, or pip3 install Flask on mac
-Add a back-end server if you want, the front-end hits port 5000
+Add a back-end server if you want, the front-end hits port 5000   
+in loadTest.js at the bottom change the file you want to write to
